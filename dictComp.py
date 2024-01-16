0683e9edd68636f363da0d2f342f5e6af1d3c6a3
TomaszSlupik <slupiktomasz@gmail.com>
2024-01-16 13:29:49 +0100

    dictComp

--- dictComp.py
+++ dictComp.py
@@ -21,3 +21,39 @@ print('---')
 
 # przestaw wartości słownika z kluczami
 stock_codes = {'Boombit': '001', 'CD Projekt': '002', 'Playway': '003'}
+
+change_stock_code  = {value: key for key, value in stock_codes.items()}
+
+print(change_stock_code)
+
+print('---')
+
+# wyodrębnij ze słownika pary klucz-wartość o wartości powyżej 100.
+stock_prices = {'Boombit': 22, 'CD Projekt': 295, 'Playway': 350}
+
+highStocksPrices = {key: value for key, value in stock_prices.items() if value > 100}
+
+print(highStocksPrices)
+
+#  listę składającą się ze słowników mapujących 
+# kolejne cyfry od 1 do 9 włącznie na ich odpowiednie 
+# k-te potęgi, dla k = 1, 2, 3 
+
+k1 = 1 
+k2 = 2
+k3 = 3
+myNumber = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9}
+myNumberList = []
+
+myNumberFirst = {key: value ** k1 for key, value in myNumber.items()}
+myNumberSecond = {key: value ** k2 for key, value in myNumber.items()}
+myNumberThird = {key: value ** k3 for key, value in myNumber.items()}
+
+myNumberList.append(myNumberFirst)
+myNumberList.append(myNumberSecond)
+myNumberList.append(myNumberThird)
+
+print(myNumberList)
+
+print('---')
+
